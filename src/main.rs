6ff9e3c39b303a9425c7565286a968092d4d2b18
David Begin <davidmichaelbe@gmail.com>
2015-05-15 21:44:44 -0700

    that is a well written and satisfying beginning
    I liked how quickly it gets you to just being able to mess around.
    I liked all the links when going over the concepts that weren't in scope
    
    I also found out the apparent () is just an empty tuple,
    which I never put together.
    
    I also learned about tuples very on in my first days experimenting in
    Rust, and haven't messed with them again, or found a need for them.
    I should find out why.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Enter your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .ok()
+        .expect("Failed to read input");
+
+    println!("Woah I have the guess! {}", guess);
 }
