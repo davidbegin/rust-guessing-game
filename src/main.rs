ae8fe092c9c61b613515ff23c87ff14e69784e23
David Begin <davidmichaelbe@gmail.com>
2015-05-15 22:11:23 -0700

    I actually had fun playing that!

--- src/main.rs
+++ src/main.rs
@@ -9,24 +9,26 @@ fn main() {
     println!("Enter your guess.");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .ok()
-        .expect("Failed to read input");
+    loop {
+        let mut guess = String::new();
 
-    let guess: u32 = guess.trim().parse()
-        .ok()
-        .expect("Failed to convert");
+        io::stdin().read_line(&mut guess)
+            .ok()
+            .expect("Failed to read input");
 
-    println!("Woah I have the guess! {}", guess);
+        let guess: u32 = guess.trim().parse()
+            .ok()
+            .expect("Failed to convert");
+
+        println!("Woah I have the guess! {}", guess);
 
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("too small!"),
-        Ordering::Equal => println!("You win!"),
-        Ordering::Greater => println!("Too Big")
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small!"),
+            Ordering::Equal => println!("You win!"),
+            Ordering::Greater => println!("Too Big")
+        }
+
     }
-
-    println!("\nSecret Number: {}\n", secret_number);
 }
