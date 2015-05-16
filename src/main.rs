f81a1976980b7a7c0d865e1b0410ae0a09184b79
David Begin <davidmichaelbe@gmail.com>
2015-05-15 21:57:51 -0700

    random is always fun, but the include of mods and traits is jarring, but how can it not be.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
     println!("Enter your guess.");
 
+    let secret_number = rand::thread_rng().gen_range(1, 101);
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess)
@@ -11,4 +15,6 @@ fn main() {
         .expect("Failed to read input");
 
     println!("Woah I have the guess! {}", guess);
+
+    println!("I can print a random number! {}", secret_number);
 }
